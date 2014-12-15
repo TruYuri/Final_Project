14586c9be6633fce48077f515a2417a641f67c2b
Austin Koehler <truyuri@outlook.com>
2014-12-14 21:16:07 -0600

    Line projectiles for machine gun

--- FinalProject/FinalProject/Projectile.cs
+++ FinalProject/FinalProject/Projectile.cs
@@ -13,6 +13,7 @@ using Microsoft.Xna.Framework.Net;
 
 namespace FinalProject
 {
+    public enum ProjectileType { Model, Line, None };
     struct ProjectileDefinition
     {
         public string name;
@@ -21,8 +22,9 @@ namespace FinalProject
         public float speed;
         public float damage;
         public float fireTime;
+        public ProjectileType type;
 
-        public ProjectileDefinition(string n, string m, float t, float s, float d, float f)
+        public ProjectileDefinition(string n, string m, float t, float s, float d, float f, ProjectileType p)
         {
             name = n;
             modelName = m;
@@ -30,6 +32,7 @@ namespace FinalProject
             speed = s;
             damage = d;
             fireTime = f;
+            type = p;
         }
     }
 
@@ -37,21 +40,31 @@ namespace FinalProject
     {
         public static Dictionary<string, ProjectileDefinition> definitions = new Dictionary<string, ProjectileDefinition>()
         {
-            { "bullet", new ProjectileDefinition("bullet", "spaceship", 20.0f, -1000.0f, 500.0f, 1.0f) },
-            { "rocket", new ProjectileDefinition("rocket", "spaceship", 20.0f, -50.0f, 500.0f, 0.1f) }
+            { "bullet", new ProjectileDefinition("bullet", "", 0.5f, -5000.0f, 500.0f, 1.0f, ProjectileType.Line) },
+            { "rocket", new ProjectileDefinition("rocket", "spaceship", 20.0f, -50.0f, 500.0f, 0.1f, ProjectileType.Model) }
         };
 
         Vector3 dir;
         Vector3 baseVelocity;
         float lifeTime;
+        VertexBuffer buffer;
 
         public Projectile(Vector3 pos, Vector3 d, Vector3 baseVel, string name, string owner)
-            : base(new BasicModel(Game1.ContentManager.Load<Model>(definitions[name].modelName), Vector3.Zero), true, name, owner)
+            : base(definitions[name].modelName == "" ? null : new BasicModel(Game1.ContentManager.Load<Model>(definitions[name].modelName), Vector3.Zero), true, name, owner)
         {
             lifeTime = 0.0f;
             dir = d;
             baseVelocity = baseVel;
             world = Matrix.CreateWorld(pos, d, Vector3.Up);
+
+            buffer = new VertexBuffer(Game1.GraphicsDeviceRef, typeof(VertexPositionColor), 2, BufferUsage.None);
+
+            var vertices = new VertexPositionColor[2];
+            vertices[0].Position = Vector3.Zero;
+            vertices[0].Color = Color.Gold;
+            vertices[1].Position = new Vector3(0.0f, 0.0f, -100.0f);
+            vertices[1].Color = Color.Gold;
+            buffer.SetData<VertexPositionColor>(vertices);
         }
 
         public override void Update(GameTime gameTime)
@@ -73,7 +86,21 @@ namespace FinalProject
 
         public override void Draw(Camera c)
         {
-            base.Draw(c);
+            var def = Projectile.definitions[type];
+            if(def.type == ProjectileType.Model)
+                base.Draw(c);
+            else if(def.type == ProjectileType.Line)
+            {
+                var basicEffect = new BasicEffect(Game1.GraphicsDeviceRef);
+                basicEffect.VertexColorEnabled = true;
+                basicEffect.Projection = c.projection;
+                basicEffect.View = c.view;
+                basicEffect.World = world;
+                basicEffect.TextureEnabled = false;
+                basicEffect.CurrentTechnique.Passes[0].Apply();
+                Game1.GraphicsDeviceRef.SetVertexBuffer(buffer);
+                Game1.GraphicsDeviceRef.DrawIndexedPrimitives(PrimitiveType.LineList, 0, 0, 2, 0, 1);
+            }
         }
     }
 }
